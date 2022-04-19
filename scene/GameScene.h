fb8e2a55e48bf90682f2dc4773d470793ae26b70
ShogoKamiota <k021g1415@g.neec.ac.jp>
2022-04-19 14:16:11 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DirectXCommon.h"
 #include "DebugText.h"
+#include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
@@ -47,8 +47,26 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+	Sprite* sprite_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
+
+	// 3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//サウンドデータハンドル
+	uint32_t soundDateHandle_ = 0;
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//値を表示したい変数
+	int32_t value_ = 0;
+
 	/// </summary>
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
 };
